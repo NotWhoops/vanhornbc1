7ba0f7c5f8ff78b9b63fb9793eced48e7afc56d3
Brandon Van Horn <bmdvanhorn@icloud.com>
2026-02-25 15:53:59 -0800

    added buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
+import { Button } from "flowbite-react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="dark:bg-white">
       <main>
-        <h1>Hello</h1>
+        <img className="align-top" src="./assets/luffy.png" alt="luffy" />
+        <div className="text-black justify-items-end">
+          <Button color="black">Home</Button>
+          <Button color="black">About</Button>
+          <Button color="black">Influential</Button>
+          <Button color="black">Enemies</Button>
+        </div>
+        <div className="dark:bg-black">
+
+        </div>
       </main>
     </div>
   );
